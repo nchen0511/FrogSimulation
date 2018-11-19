dbc317958c379d2678eca1d0bfc82d3e5214d1a6
BT_1N3_25 <n>
2018-11-19 09:37:08 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -8,7 +8,7 @@ public class FrogSimulation {
     }
 
     private int hopDistance(){
-        return (int)(Math.random()*11);
+        return (int)(Math.random()*31)-15;
     }
 
     public boolean simulate(){
